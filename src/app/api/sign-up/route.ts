a5a50185df84e66123d54c6b3c355230fecc1a07
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-04 23:29:19 +0530

    feat: existing-user-algorithm

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -3,12 +3,71 @@ import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { messageSchema } from "@/schemas/messageSchema";
 
 export async function POST(request: Request){
     await dbConnect()
     try{
         const {username, email, password} = await request.json()
         // await UserModel;
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        if (existingUserVerifiedByUsername){
+            return Response.json({
+                success: false,
+                message: "Username is already taken"
+                
+            }, {status:400})
+        }
+
+        const existingUserByEmail = await UserModel.findOne ({email})
+
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        if (existingUserByEmail){
+            true //todo
+        }
+        else {
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const expiryDate= new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: [],
+
+            })
+
+            await newUser.save()
+        }
+        //send verification email
+        const emailResponse = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode,
+        )
+
+        if(!emailResponse.success){
+            return Response.json({
+                success: false,
+                messageSchema: emailResponse.message
+            }, {status: 500})
+
+        }
+
+        return Response.json({
+            success: true,
+            messageSchema: "User registered successfully, please verify your emai"
+        }, {status: 201})
 
     } catch(error){
         console.error('Error registering user', error)
